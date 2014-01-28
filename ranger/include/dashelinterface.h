1718b13edf8feedc968058df7704ba66e2c9b7bf
David Mansolino <david.mansolino@epfl.ch>
2014-01-28 14:09:27 +0100

    Corrected indentation of DashelInterface.hpp

--- ranger/include/dashelinterface.h
+++ ranger/include/dashelinterface.h
@@ -18,77 +18,77 @@
 #include "../common/msg/descriptions-manager.h"
 
 struct packet {
-	int id;
-	int dist;
-	int angle;
-	int data[7];
+  int id;
+  int dist;
+  int angle;
+  int data[7];
 };
 
 class DashelInterface : public Dashel::Hub, public Aseba::DescriptionsManager
 {
-	public:
-        typedef std::map<std::string, Aseba::Compiler::VariablesMap> NodeNameToVariablesMap;
+  public:
+    typedef std::map<std::string, Aseba::Compiler::VariablesMap> NodeNameToVariablesMap;
 
-		DashelInterface();
-		~DashelInterface();
-		void connectAseba(const char * target);
-		void disconnectAseba();
-		void dashelConnect();
-		void dashelRun();
-		bool dashelConnected();
-        bool getNodeAndVarPos(const std::string& nodeName, const std::string& variableName, unsigned& nodeId, unsigned& pos) const;
+    DashelInterface();
+    ~DashelInterface();
+    void connectAseba(const char * target);
+    void disconnectAseba();
+    void dashelConnect();
+    void dashelRun();
+    bool dashelConnected();
+    bool getNodeAndVarPos(const std::string& nodeName, const std::string& variableName, unsigned& nodeId, unsigned& pos) const;
 
-        void sendEvent(const char * event, const int * args, int argsNumber);
+    void sendEvent(const char * event, const int * args, int argsNumber);
 
-        int * getSharpValues();
-        int * getAccelerometerValues();
-        int * getEncodersValues();
-        int   getBatteryLevel();
-        int   getBalance();
-        int   getBumper();
-        int * getSpeed();
-        int * getTouchSensors();
-        int   getLolette();
-        int   getChargingState();
-        int * getMotorsCurrent();
+    int * getSharpValues();
+    int * getAccelerometerValues();
+    int * getEncodersValues();
+    int   getBatteryLevel();
+    int   getBalance();
+    int   getBumper();
+    int * getSpeed();
+    int * getTouchSensors();
+    int   getLolette();
+    int   getChargingState();
+    int * getMotorsCurrent();
 
-        std::vector<packet> * getPackets();
-        void cleanPackets();
+    std::vector<packet> * getPackets();
+    void cleanPackets();
 
-	protected:
-        Aseba::CommonDefinitions commonDefinitions;
-        NodeNameToVariablesMap allVariables;
+  protected:
+    Aseba::CommonDefinitions commonDefinitions;
+    NodeNameToVariablesMap allVariables;
 
-        virtual void nodeDescriptionReceived(unsigned nodeId);
+    virtual void nodeDescriptionReceived(unsigned nodeId);
 
-		// to be called by pthread
-		static void * run(void *param);
+    // to be called by pthread
+    static void * run(void *param);
 
-		// from Dashel::Hub
-		virtual void incomingData(Dashel::Stream *stream);
-		virtual void connectionClosed(Dashel::Stream *stream, bool abnormal);
+    // from Dashel::Hub
+    virtual void incomingData(Dashel::Stream *stream);
+    virtual void connectionClosed(Dashel::Stream *stream, bool abnormal);
 
-		// members
-		Dashel::Stream* stream;
-		const char * dashelParams;
-		bool isRunning;
-		bool isConnected;
+    // members
+    Dashel::Stream* stream;
+    const char * dashelParams;
+    bool isRunning;
+    bool isConnected;
 
-        int                 mSharpValues[5];
-        int                 mAccelValues[3];
-        int                 mEncoders[4];
-        int                 mBattery;
-        int                 mBalance;
-        int                 mBumper;
-        int                 mSpeed[2];
-        int                 mTouch[27];
-        int                 mMotorsCurrent[2];
-        int                 mLolette;
-        int                 mChargingState;
-        std::vector<packet> mPackets;
-        pthread_t           mDashelThread;
+    int                 mSharpValues[5];
+    int                 mAccelValues[3];
+    int                 mEncoders[4];
+    int                 mBattery;
+    int                 mBalance;
+    int                 mBumper;
+    int                 mSpeed[2];
+    int                 mTouch[27];
+    int                 mMotorsCurrent[2];
+    int                 mLolette;
+    int                 mChargingState;
+    std::vector<packet> mPackets;
+    pthread_t           mDashelThread;
         
-        void decompressTouch(int right, int left, int rear);
+    void decompressTouch(int right, int left, int rear);
 };
 
 
