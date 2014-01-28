a07450900ee9ec36b5e398e34d3b3c8a3518159d
David Mansolino <david.mansolino@epfl.ch>
2014-01-28 13:54:41 +0100

    Corrected indentation in Receiver.cpp

--- ranger/src/Receiver.cpp
+++ ranger/src/Receiver.cpp
@@ -22,24 +22,24 @@ const double* Receiver::getEmitterDirection() const {
 }
 
 double Receiver::getSignalStrength() const {
-	return mPacket[mPacket.size()].getSignalStrength();
+  return mPacket[mPacket.size()].getSignalStrength();
 }
 
 int Receiver::getQueueLength() const {
-	return mPacket.size();
+  return mPacket.size();
 }
 
 const void * Receiver::getData() const {
-	return mPacket[mPacket.size()].getPacketData();
+  return mPacket[mPacket.size()].getPacketData();
 }
 
 void  Receiver::nextPacket() {
-	if(mPacket.size() > 0)
-	  mPacket.pop_back();
+  if(mPacket.size() > 0)
+    mPacket.pop_back();
 }
 
 int Receiver::getEmitterID() const {
-	return mPacket[mPacket.size()].getEmitterID();
+  return mPacket[mPacket.size()].getEmitterID();
 }
 
 void Receiver::resetQueue() {
@@ -48,6 +48,6 @@ void Receiver::resetQueue() {
 }
 
 void Receiver::addPacket(int id, int distance, int angle, int * data) {
-	Packet newPacket(id, distance, angle, data);
-    mPacket.push_front(newPacket);
+  Packet newPacket(id, distance, angle, data);
+   mPacket.push_front(newPacket);
 }
