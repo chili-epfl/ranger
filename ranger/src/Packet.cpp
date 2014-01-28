d0877b0c7cd58f9b5f32d0db797ff2727600260d
David Mansolino <david.mansolino@epfl.ch>
2014-01-28 13:54:04 +0100

    Corrected indentation in Packet.cpp

--- ranger/src/Packet.cpp
+++ ranger/src/Packet.cpp
@@ -5,40 +5,40 @@ using namespace webots;
 
 Packet::Packet(int id, int distance, int angle, int * data)
 {
-	mEmitterDirection[0] = sin(M_PI * ((float)angle / 1800));
-    mEmitterDirection[1] = 0;
-    mEmitterDirection[2] = cos(M_PI * ((float)angle / 1800));
-    mSignalStrength = 1.0 / (((float)distance/1000) * ((float)distance/1000));
-    mEmitterID = id;
-    for(int c=0; c<7; ++c)
-		mData[c] = data[c];
+  mEmitterDirection[0] = sin(M_PI * ((float)angle / 1800));
+  mEmitterDirection[1] = 0;
+  mEmitterDirection[2] = cos(M_PI * ((float)angle / 1800));
+  mSignalStrength = 1.0 / (((float)distance/1000) * ((float)distance/1000));
+  mEmitterID = id;
+  for(int c=0; c<7; ++c)
+    mData[c] = data[c];
 }
 
 Packet::~Packet() {
 }
 
 const double* Packet::getEmitterDirection() const {
-   return mEmitterDirection;
+  return mEmitterDirection;
 }
 
 double Packet::getSignalStrength() const {
-	return mSignalStrength;
+  return mSignalStrength;
 }
 
 int Packet::getEmitterID() const {
-	return mEmitterID;
+  return mEmitterID;
 }
 
 const void * Packet::getPacketData() const {
-	return mData;
+  return mData;
 }
 
 void Packet::resetPacket() {
-	mEmitterDirection[0] = 0;
-    mEmitterDirection[1] = 0;
-    mEmitterDirection[2] = 0;
-    mSignalStrength = 0;
-    mEmitterID = -1;
-    for(int c=0; c<7; ++c)
-		mData[c] = 0;
+  mEmitterDirection[0] = 0;
+  mEmitterDirection[1] = 0;
+  mEmitterDirection[2] = 0;
+  mSignalStrength = 0;
+  mEmitterID = -1;
+  for(int c=0; c<7; ++c)
+    mData[c] = 0;
 }
