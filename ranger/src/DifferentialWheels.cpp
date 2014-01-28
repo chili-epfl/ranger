1718e1bcd06b098985eef92e7ffdaea5e2a1f352
David Mansolino <david.mansolino@epfl.ch>
2014-01-28 13:51:15 +0100

    Corrected indentation in DifferentialWheels.cpp

--- ranger/src/DifferentialWheels.cpp
+++ ranger/src/DifferentialWheels.cpp
@@ -19,40 +19,40 @@
 using namespace std;
 
 webots::DifferentialWheels::DifferentialWheels() {
-    dashelInterface = new DashelInterface();
-    initRanger();
-    initDevices();
-    usleep(1000000);
-    mPreviousStepTime = 0.0;
-    gettimeofday(&mStartTime, NULL);
-    mTargetSpeed[0] = NAN;
-    mTargetSpeed[1] = NAN;
-    mEncoderEnable = false;
-    mRightEncoder = 0;
-    mLeftEncoder = 0;
-    mRightCurrent = 0;
-    mLeftCurrent = 0;
-    mVideoIsPlaying = true; // we don't know but it could be
-    if(!dashelInterface->dashelConnected())
-		exit(-1);
-    const int args[1] = {1};
-	dashelInterface->sendEvent("enableFeedback", args, 1);
-	// half open eyes and eyes in the center
-	((Motor *)mDevices["LeftUpperEyelid"])->setPosition(M_PI_4);
-	((Motor *)mDevices["LeftLowerEyelid"])->setPosition(M_PI_4);
-	((Motor *)mDevices["RightUpperEyelid"])->setPosition(M_PI_4);
-	((Motor *)mDevices["RightLowerEyelid"])->setPosition(M_PI_4);
-	((Motor *)mDevices["RightPupilVert"])->setPosition(0);
-	((Motor *)mDevices["RightPupilHori"])->setPosition(0);
-	((Motor *)mDevices["LeftPupilVert"])->setPosition(0);
-	((Motor *)mDevices["LeftPupilHori"])->setPosition(0);
+  dashelInterface = new DashelInterface();
+  initRanger();
+  initDevices();
+  usleep(1000000);
+  mPreviousStepTime = 0.0;
+  gettimeofday(&mStartTime, NULL);
+  mTargetSpeed[0] = NAN;
+  mTargetSpeed[1] = NAN;
+  mEncoderEnable = false;
+  mRightEncoder = 0;
+  mLeftEncoder = 0;
+  mRightCurrent = 0;
+  mLeftCurrent = 0;
+  mVideoIsPlaying = true; // we don't know but it could be
+  if(!dashelInterface->dashelConnected())
+    exit(-1);
+  const int args[1] = {1};
+  dashelInterface->sendEvent("enableFeedback", args, 1);
+  // half open eyes and eyes in the center
+  ((Motor *)mDevices["LeftUpperEyelid"])->setPosition(M_PI_4);
+  ((Motor *)mDevices["LeftLowerEyelid"])->setPosition(M_PI_4);
+  ((Motor *)mDevices["RightUpperEyelid"])->setPosition(M_PI_4);
+  ((Motor *)mDevices["RightLowerEyelid"])->setPosition(M_PI_4);
+  ((Motor *)mDevices["RightPupilVert"])->setPosition(0);
+  ((Motor *)mDevices["RightPupilHori"])->setPosition(0);
+  ((Motor *)mDevices["LeftPupilVert"])->setPosition(0);
+  ((Motor *)mDevices["LeftPupilHori"])->setPosition(0);
 }
 
 webots::DifferentialWheels::~DifferentialWheels() {
-	const int args[1] = {0};
-	dashelInterface->sendEvent("enableFeedback", args, 1);
-	usleep(100000);
-    dashelInterface->disconnectAseba();
+  const int args[1] = {0};
+  dashelInterface->sendEvent("enableFeedback", args, 1);
+  usleep(100000);
+  dashelInterface->disconnectAseba();
 }
 
 int webots::DifferentialWheels::step(int ms) {
@@ -73,15 +73,15 @@ int webots::DifferentialWheels::step(int ms) {
   ((TouchSensor *)mDevices["balance"])->setValue(dashelInterface->getBalance());
   // Touch pannels
   for(int c=0; c<9; ++c) {
-	  // right pannel
-	  sprintf(buffer, "touchR%d", c);
-	  ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c]);
-	  // rear pannel
-	  sprintf(buffer, "touchB%d", c);
-	  ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c+9]);
-	  // left pannel
-	  sprintf(buffer, "touchL%d", c);
-	  ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c+18]);
+    // right pannel
+    sprintf(buffer, "touchR%d", c);
+    ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c]);
+    // rear pannel
+    sprintf(buffer, "touchB%d", c);
+    ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c+9]);
+    // left pannel
+    sprintf(buffer, "touchL%d", c);
+    ((TouchSensor *)mDevices[buffer])->setValue(dashelInterface->getTouchSensors()[c+18]);
   }
   // Battery
   mBatterylevel = dashelInterface->getBatteryLevel();
@@ -94,13 +94,13 @@ int webots::DifferentialWheels::step(int ms) {
   mLeftCurrent  = dashelInterface->getMotorsCurrent()[1];
   // Wheel encoders
   if(mEncoderEnable) {
-	  const int * encodersState = dashelInterface->getEncodersValues();
-	  if(encodersState[0] > 0)
-		mRightEncoder = encodersState[0] + 65536*encodersState[1]; // 65536 = 2^16 = maximum value representable with 16bits 
-      else 
+    const int * encodersState = dashelInterface->getEncodersValues();
+    if(encodersState[0] > 0)
+      mRightEncoder = encodersState[0] + 65536*encodersState[1]; // 65536 = 2^16 = maximum value representable with 16bits 
+    else 
         mRightEncoder = 65536 + encodersState[0] + 65536*encodersState[1];
      if(encodersState[2] > 0)
-		mLeftEncoder = -(encodersState[2] + 65536*encodersState[3]);
+        mLeftEncoder = -(encodersState[2] + 65536*encodersState[3]);
       else 
         mLeftEncoder = -(65536 + encodersState[2] + 65536*encodersState[3]);
   }
@@ -131,17 +131,17 @@ int webots::DifferentialWheels::step(int ms) {
   }
   // Motors
   if(Motor::hasChanged()) {
-	  int args[8];
-	  args[0] = 100 * (((Motor *)mDevices["LeftPupilHori"])->getTargetPosition()    / ((Motor *)mDevices["LeftPupilHori"])->getMaxPosition());
-	  args[1] = 100 * (((Motor *)mDevices["LeftPupilVert"])->getTargetPosition()    / ((Motor *)mDevices["LeftPupilVert"])->getMaxPosition());
-	  args[2] = 100 * (((Motor *)mDevices["RightPupilHori"])->getTargetPosition()   / ((Motor *)mDevices["RightPupilHori"])->getMaxPosition());
-	  args[3] = 100 * (((Motor *)mDevices["RightPupilVert"])->getTargetPosition()   / ((Motor *)mDevices["RightPupilVert"])->getMaxPosition());
-	  args[4] = 100 * (((Motor *)mDevices["LeftUpperEyelid"])->getTargetPosition()  / ((Motor *)mDevices["LeftUpperEyelid"])->getMaxPosition());
-	  args[5] = 100 * (((Motor *)mDevices["LeftLowerEyelid"])->getTargetPosition()  / ((Motor *)mDevices["LeftLowerEyelid"])->getMaxPosition());
-	  args[6] = 100 * (((Motor *)mDevices["RightLowerEyelid"])->getTargetPosition() / ((Motor *)mDevices["RightLowerEyelid"])->getMaxPosition());
-	  args[7] = 100 * (((Motor *)mDevices["RightUpperEyelid"])->getTargetPosition() / ((Motor *)mDevices["RightUpperEyelid"])->getMaxPosition());
-      dashelInterface->sendEvent("neuilEvent", (const int *)args, 8);
-	  Motor::resetChanged();
+    int args[8];
+    args[0] = 100 * (((Motor *)mDevices["LeftPupilHori"])->getTargetPosition()    / ((Motor *)mDevices["LeftPupilHori"])->getMaxPosition());
+    args[1] = 100 * (((Motor *)mDevices["LeftPupilVert"])->getTargetPosition()    / ((Motor *)mDevices["LeftPupilVert"])->getMaxPosition());
+    args[2] = 100 * (((Motor *)mDevices["RightPupilHori"])->getTargetPosition()   / ((Motor *)mDevices["RightPupilHori"])->getMaxPosition());
+    args[3] = 100 * (((Motor *)mDevices["RightPupilVert"])->getTargetPosition()   / ((Motor *)mDevices["RightPupilVert"])->getMaxPosition());
+    args[4] = 100 * (((Motor *)mDevices["LeftUpperEyelid"])->getTargetPosition()  / ((Motor *)mDevices["LeftUpperEyelid"])->getMaxPosition());
+    args[5] = 100 * (((Motor *)mDevices["LeftLowerEyelid"])->getTargetPosition()  / ((Motor *)mDevices["LeftLowerEyelid"])->getMaxPosition());
+    args[6] = 100 * (((Motor *)mDevices["RightLowerEyelid"])->getTargetPosition() / ((Motor *)mDevices["RightLowerEyelid"])->getMaxPosition());
+    args[7] = 100 * (((Motor *)mDevices["RightUpperEyelid"])->getTargetPosition() / ((Motor *)mDevices["RightUpperEyelid"])->getMaxPosition());
+    dashelInterface->sendEvent("neuilEvent", (const int *)args, 8);
+    Motor::resetChanged();
   }
 
   // -------- Timing management -------- //
@@ -181,43 +181,43 @@ std::string webots::DifferentialWheels::getName() const {
 }
 
 double webots::DifferentialWheels::getMaxSpeed() const {
-	return 19.2;
+  return 19.2;
 }
 
 double webots::DifferentialWheels::getSpeedUnit() const {
-	return 0.0144;
+  return 0.0144;
 }
 
 double webots::DifferentialWheels::getLeftSpeed() const {
-	return mLeftSpeed;
+  return mLeftSpeed;
 }
 
 double webots::DifferentialWheels::getRightSpeed() const {
-	return mRightSpeed;
+  return mRightSpeed;
 }
 
 void webots::DifferentialWheels::enableEncoders(int ms) {
-	if(mEncoderEnable == false) {
-		const int args[1] = {1};
-		dashelInterface->sendEvent("enableEncoders", args, 1);
-		mEncoderEnable = true;
-	}
+  if(mEncoderEnable == false) {
+    const int args[1] = {1};
+    dashelInterface->sendEvent("enableEncoders", args, 1);
+    mEncoderEnable = true;
+  }
 }
 
 void webots::DifferentialWheels::disableEncoders() {
-	if(mEncoderEnable == true) {
-		const int args[1] = {0};
-		dashelInterface->sendEvent("enableEncoders", args, 1);
-		mEncoderEnable = false;
-	}
+if(mEncoderEnable == true) {
+    const int args[1] = {0};
+    dashelInterface->sendEvent("enableEncoders", args, 1);
+    mEncoderEnable = false;
+  }
 }
 
 double webots::DifferentialWheels::getLeftEncoder() const {
-	return mLeftEncoder;
+  return mLeftEncoder;
 }
 
 double webots::DifferentialWheels::getRightEncoder() const {
-	return mRightEncoder;
+  return mRightEncoder;
 }
 
 void webots::DifferentialWheels::batterySensorEnable(int ms) {
@@ -229,32 +229,32 @@ void webots::DifferentialWheels::batterySensorDisable() {
 }
 
 double webots::DifferentialWheels::batterySensorGetValue() {
-	return mBatterylevel;
+  return mBatterylevel;
 }
 
 void webots::DifferentialWheels::connectToDashel() {
-    dashelInterface->connectAseba("tcp:127.0.0.1;33333");
+  dashelInterface->connectAseba("tcp:127.0.0.1;33333");
 }
 
 void webots::DifferentialWheels::setSpeed(double left, double right) {
-	if((mTargetSpeed[1] != left) || (mTargetSpeed[0] != right)) {
-		mTargetSpeed[0] = right;
-		mTargetSpeed[1] = left;
-		if(right < -100)
-		  right = -100;
-		else if(right > 100)
-		  right = 100;
-		if(left < -100)
-		  left = -100;
-		else if(left > 100)
-		  left = 100;
-		const int args[2] = {(int)right, (int)-left};
-		dashelInterface->sendEvent("setSpeed", args, 2);
-	}
+  if((mTargetSpeed[1] != left) || (mTargetSpeed[0] != right)) {
+    mTargetSpeed[0] = right;
+    mTargetSpeed[1] = left;
+    if(right < -100)
+      right = -100;
+    else if(right > 100)
+      right = 100;
+    if(left < -100)
+      left = -100;
+    else if(left > 100)
+      left = 100;
+    const int args[2] = {(int)right, (int)-left};
+    dashelInterface->sendEvent("setSpeed", args, 2);
+  }
 }
 
 void webots::DifferentialWheels::initRanger() {
-    connectToDashel();
+  connectToDashel();
 }
 
 webots::Device *webots::DifferentialWheels::getDevice(const std::string &name) const {
@@ -350,15 +350,15 @@ void webots::DifferentialWheels::initDevices() {
   mDevices["balance"]          = new webots::TouchSensor("balance", this,webots::TouchSensor::FORCE); 
   // Touch pannel
   for(int c=0; c<9; ++c) {
-	  // right pannel
-	  sprintf(buffer, "touchR%d", c);
-	  mDevices[buffer]          = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
-	  // rear pannel
-	  sprintf(buffer, "touchB%d", c);
-	  mDevices[buffer]          = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
-	  // left pannel
-	  sprintf(buffer, "touchL%d", c);
-	  mDevices[buffer]          = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
+    // right pannel
+    sprintf(buffer, "touchR%d", c);
+    mDevices[buffer]           = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
+    // rear pannel
+    sprintf(buffer, "touchB%d", c);
+    mDevices[buffer]           = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
+    // left pannel
+    sprintf(buffer, "touchL%d", c);
+    mDevices[buffer]           = new webots::TouchSensor(buffer, this, webots::TouchSensor::BUMPER);
   }
   
   mDevices["RightPupilVert"]   = new webots::Motor("RightPupilVert", this, -0.028, 0.028);
@@ -373,40 +373,40 @@ void webots::DifferentialWheels::initDevices() {
 }
 
 void webots::DifferentialWheels::playLedVideo(int videoNumber, bool repeat) {
-	mVideoIsPlaying = true;
-	if(repeat) {
-		const int args[2] = {videoNumber, 1};
-		dashelInterface->sendEvent("playLedVid", args, 2);
-	}
-	else {
-		const int args[2] = {videoNumber, 0};
-		dashelInterface->sendEvent("playLedVid", args, 2);
-	}
+  mVideoIsPlaying = true;
+  if(repeat) {
+    const int args[2] = {videoNumber, 1};
+    dashelInterface->sendEvent("playLedVid", args, 2);
+  }
+  else {
+    const int args[2] = {videoNumber, 0};
+    dashelInterface->sendEvent("playLedVid", args, 2);
+  }
 }
 
 void webots::DifferentialWheels::stopLedVideo() {
-	dashelInterface->sendEvent("stopLedVid", NULL, 0);
-	mVideoIsPlaying = false;
+  dashelInterface->sendEvent("stopLedVid", NULL, 0);
+  mVideoIsPlaying = false;
 }
 
 void webots::DifferentialWheels::emitCustomEvent(int *tab) {
-	const int * args = tab;
-	dashelInterface->sendEvent("customEvent", args, 10);
+  const int * args = tab;
+  dashelInterface->sendEvent("customEvent", args, 10);
 }
 
             
 bool webots::DifferentialWheels::isLolettePresent() {
-	return mLolette;
+  return mLolette;
 }
 
 bool webots::DifferentialWheels::isCharging() {
-	return mIsCharging;
+  return mIsCharging;
 }
 
 int webots::DifferentialWheels::getRightMotorCurrent() {
-	return mRightCurrent;
+  return mRightCurrent;
 }
 
 int webots::DifferentialWheels::getLeftMotorCurrent() {
-	return mLeftCurrent;
+  return mLeftCurrent;
 }
