1b0df8184d8eb741f027c797ec547ce29f783e53
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-02-17 16:56:46 +0100

    Updated script and instruction to connect to Ranger2
    Includes some tips to establish the Bluetooth connection.

--- tools/ranger_connect.sh
+++ tools/ranger_connect.sh
@@ -1,9 +1,40 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo -e "*** Configuring bluetooth ***"
-hcitool scan
-sudo rfcomm bind 0 10:00:E8:6C:F0:4F
+# set here the Bluetooth address of the robot bt2CAN adapter
+BT_ADDRESS='10:00:E8:6C:F0:4F'
 
-echo -e "\n*** Starting AsebaSwitch ***"
-cd /home/david/aseba/build-aseba/switches/switch
-sudo ./asebaswitch "ser:device=/dev/rfcomm0;fc=hard;baud=921600"
+# if you want to use a specific local bluetooth adapter, 
+# specify here its BT address
+LOCAL_BT_INTERFACE='hci0'
+
+EXEC='asebamedulla'
+#EXEC='asebaswitch'
+
+RFCOMM='0'
+device="/dev/rfcomm$RFCOMM"
+
+# bind to rfcomm
+if [ -e $device ]; then 
+        echo "$device already exists. Fine, using it."
+else
+        if [[ -n $LOCAL_BT_INTERFACE ]]; then 
+            echo "Binding to $device from local adapter $LOCAL_BT_INTERFACE ..."
+            sudo rfcomm -i $LOCAL_BT_INTERFACE bind $RFCOMM $BT_ADDRESS
+        else
+            echo "Binding to $device ..."
+            sudo rfcomm bind $RFCOMM $BT_ADDRESS
+        fi
+fi
+
+# $# is the nb of args -> if any arg is given (like '--debug'), run in debug mode
+if [ $# -eq 0 ] 
+then
+    echo "Connecting to target..."
+    $EXEC "ser:device=$device;fc=hard;baud=921600"
+    echo "Connection successfully established! You can now talk to your robot :-)"
+else
+    # debug mode
+    echo "Running in debug mode"
+    $EXEC -d -v "ser:device=$device;fc=hard;baud=921600"
+fi
+
